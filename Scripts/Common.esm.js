5e471fffcbee7e32d2350a57f6873f167fb5209b
Mateusz Rosiński <matirosinski02@gmail.com>
2022-03-25 23:53:09 +0100

    Added class which answer for finding elements

---
+++ Scripts/Common.esm.js
@@ -0,0 +1,29 @@
+export const SCREEN_OBJECT = {
+  HIDDEN_CLASS: 'hidden',
+  HIDDEN_SCREEN: false,
+  VISIBLE_SCREEN: true
+}
+
+// Class which answer for finding elements in game
+class Common {
+  constructor(elementId) {
+    this.element = this.bindToElement(elementId)
+  }
+
+  bindToElement = elementFindById => {
+    const element = document.getElementById(elementFindById);
+    if(!element) {
+      throw new Error(`Not found element id: ${elementFindById}`);
+
+    } else return element
+    
+  }
+
+  changeScreen = (element, mode) => {
+    mode === SCREEN_OBJECT.VISIBLE_SCREEN 
+      ? element.classList.remove(SCREEN_OBJECT.HIDDEN_CLASS)
+    : element.classList.add(SCREEN_OBJECT.HIDDEN_CLASS)
+  }
+}
+
+export default Common
