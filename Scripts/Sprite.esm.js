e3a7e2a802d8c75ccaaa88b6d11c833aa2a5367a
Mateusz Rosiński <matirosinski02@gmail.com>
2022-03-29 22:55:15 +0200

    Added class which service sprite images

---
+++ Scripts/Sprite.esm.js
@@ -0,0 +1,40 @@
+import { canvas } from "./Canvas.esm.js";
+
+class Sprite {
+  constructor(x, y, width, height, spriteImage, numberOfSprites = 1, offset = {x: 0, y: 0}) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.spriteImage = spriteImage;
+    this.numberOfSprites = numberOfSprites;
+    this.offset = {...offset};
+    this.alpha = 255;
+  }
+
+  draw = (numberOfSprites = 0, ratio = 1) => {
+    if(numberOfSprites > this.numberOfSprites) {
+      return;
+    }
+
+    if(this.alpha !== 255) {
+      canvas.context.globalAlpha = this.alpha / 255;
+    }
+
+    canvas.context.drawImage(
+      this.spriteImage,
+      numberOfSprites * this.width,
+      0,
+      this.width,
+      this.height,
+      this.x + this.offset.x,
+      this.y + this.offset.y,
+      this.width * ratio,
+      this.height * ratio,
+    )
+
+    if(this.alpha !== 255) {
+      canvas.context.globalAlpha = 1;
+    }
+  }
+}
